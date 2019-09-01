85d4cca5c344ddb2ca108fcae655c1b9a5bc8a82
Felix Palta <fpalta90@gmail.com>
2019-09-01 23:25:33 +0300

    Add ip printer class

--- main.cpp
+++ main.cpp
@@ -18,34 +18,41 @@ template <typename T, typename Alloc>
 struct is_sequential_container<std::list<T, Alloc>> : public std::true_type
 {};
 
-template <typename T, std::enable_if_t<std::is_integral<T>::value, void*> = nullptr >
-void PrintIp(T val) {
-    std::cout << val << std::endl;
-}
+struct IpPrinter
+{
+    IpPrinter(std::ostream & os) : _os(os) {}
 
-void PrintIp(const std::string & ip) {
-    std::cout << ip << std::endl;
-}
-
-template <typename T, std::enable_if_t<is_sequential_container<T>::value, void*> = nullptr>
-void PrintIp(T val) {
-    bool first = true;
-    for (auto c : val) {
-        if (first)
-            first = false;
-        else
-            std::cout << ".";
-        std::cout << c;
+    template <typename T, std::enable_if_t<std::is_integral<T>::value, void*> = nullptr >
+    void print(T val) {
+        _os << val << std::endl;
     }
-    std::cout << std::endl;
-}
+
+    void print(const std::string & ip) {
+        _os << ip << std::endl;
+    }
+
+    template <typename T, std::enable_if_t<is_sequential_container<T>::value, void*> = nullptr>
+    void print(T val) {
+        bool first = true;
+        for (auto c : val) {
+            if (first)
+                first = false;
+            else
+                _os << ".";
+            _os << c;
+        }
+        _os << std::endl;
+    }
+private:
+    std::ostream & _os;
+};
 
 int main() {
-
-    PrintIp(99);
-    PrintIp("wololo");
-    PrintIp(std::vector<int>{1, 2, 3, 99});
-    PrintIp(std::list<char>{'a', 'b', 'c'});
+    IpPrinter p(std::cout);
+    p.print(99);
+    p.print("wololo");
+    p.print(std::vector<int>{1, 2, 3, 99});
+    p.print(std::list<char>{'a', 'b', 'c'});
 
     return 0;
 }
