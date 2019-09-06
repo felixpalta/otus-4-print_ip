a0239871fb2d65630bbd92c0eb65c869dbb5e489
Felix Palta (office) <fpalta90@gmail.com>
2019-09-06 17:17:29 +0300

    Add more tests

--- tests_default.cpp
+++ tests_default.cpp
@@ -24,3 +24,21 @@ TEST_F(OssFixture, IntTest) {
     p.print(int(std::numeric_limits<int>::max()));
     ASSERT_EQ("127.255.255.255", oss.str());
 }
+
+TEST_F(OssFixture, StringTest) {
+    auto s = "foobar";
+    p.print(s);
+    ASSERT_EQ(s, oss.str());
+}
+
+TEST_F(OssFixture, VectorTest) {
+    std::vector<int> v = {127, 0, 0 ,1};
+    p.print(v);
+    ASSERT_EQ("127.0.0.1", oss.str());
+}
+
+TEST_F(OssFixture, ListTest) {
+    std::list<int> l = {192, 168,0, 1};
+    p.print(l);
+    ASSERT_EQ("192.168.0.1", oss.str());
+}
